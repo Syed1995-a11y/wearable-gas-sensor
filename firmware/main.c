a7d1acffd98b9b6f1f43dc4d906f22791401d73f
Syed1995-a11y <snaqvi006@gmail.com>
2025-08-17 13:54:58 -0400

    Add placeholder firmware file

---
+++ firmware/main.c
@@ -0,0 +1,4 @@
+// Placeholder for ESP32 firmware code
+// This program will handle gas sensor readings and display logic
+// Planned sensors: CO, CO2, H2S, CH4
+// Microcontroller: LILYGO T-Display S3 (ESP32-S3)
